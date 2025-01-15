b26818e04eb62ad8cb95e9b52fd3a131be58998d
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-01-16 02:51:56 +0300

    feat:add my myIncludes and fix myIndexOf function

--- arraysAndTheirsMethods/index.js
+++ arraysAndTheirsMethods/index.js
@@ -1,11 +1,28 @@
 const arr = [1, 2, 3, 4, 5];
 function myIndexOf(arr, item, from) {
-  const searchPoint = from ? from - 1 : arr.length;
-  for (let i = 0; i < searchPoint; i++) {
+  const searchPoint = from ? Number(from) : 0;
+  if (searchPoint >= arr.length || searchPoint < 0) {
+    return -1;
+  }
+  for (let i = searchPoint; i < arr.length; i++) {
     if (arr[i] === item) {
       return i;
     }
   }
   return -1;
 }
-console.log(myIndexOf(arr, 4, 5));
+function myIncludes(arr, item, from) {
+  let searchPoint = from ? Number(from) : 0;
+  if (searchPoint >= arr.length) {
+    return false;
+  }
+  if (searchPoint < 0) {
+    searchPoint = 0;
+  }
+  for (let i = searchPoint; i < arr.length; i++) {
+    if (arr[i] === item) {
+      return true;
+    }
+  }
+  return false;
+}
