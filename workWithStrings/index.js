6cc4de3af6d4ff3e9c56cf973f65d299b585dd40
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-01-19 19:05:12 +0300

    feat:implement string slicer

--- workWithStrings/index.js
+++ workWithStrings/index.js
@@ -1,4 +1,8 @@
 function capitalizeFirstLetter(str) {
   return str.replace(/[a-zA-Z]/, (e) => e.toUpperCase());
 }
-console.log(capitalizeFirstLetter("string"));
+function stringSlicer(str) {
+  return str.replace(/^(.{40})(.*?)([\s,.!?:;].*)?$/, (_, p1, p2, p3) =>
+    p3 ? p1 + p2 + "..." : str
+  );
+}
